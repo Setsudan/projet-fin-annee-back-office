90d6041cd4b77be4bec401bd97e71c820a7b8924
A_Rijenth <rijenth93100@laposte.net>
2023-06-26 21:33:41 +0200

    ⚡️: fix HMR windows frontend container

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+const nextConfig = {
+    webpack: function (config, context) {
+        if(process.env.NEXT_WEBPACK_USEPOLLING) {
+            config.watchOptions = {
+              poll: 500,
+              aggregateTimeout: 300
+            }
+          }
+          return config
+    },
+};
 
 module.exports = nextConfig;
