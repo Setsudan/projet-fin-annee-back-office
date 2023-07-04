f9e3e253967a41ad2c877a398de44455630866c0
Setsu | A.K.A EthLny <60669080+Setsudan@users.noreply.github.com>
2023-07-04 10:57:26 +0200

    ♻️ : Refacto Session File

--- frontend/app/_utils/session.ts
+++ frontend/app/_utils/session.ts
@@ -2,69 +2,74 @@ import PocketBase from "pocketbase";
 
 const pb = new PocketBase("http://127.0.0.1:8090");
 
-export const loginFunc = async (username: string, password: string) => {
-  const authData = await pb
-    .collection("users")
-    .authWithPassword(username, password);
-
-  if (authData) {
-    console.log(pb.authStore.model);
-    return authData;
-  }
-};
-
-export const authStore = pb.authStore;
-
-export const isSessionValidFunc = async (): Promise<boolean> => {
-  return pb.authStore.isValid;
-};
-
-export const registerFunc = async ({
-  email,
-  password,
-  passwordConfirm,
-  first_name,
-  last_name,
-  role,
-}: {
+interface UserData {
+  username: string;
   email: string;
   password: string;
   passwordConfirm: string;
   first_name: string;
   last_name: string;
   role: string;
-}) => {
-  const username = first_name + "_" + last_name;
+}
 
-  const data = {
-    username: username,
-    email: email,
-    password: password,
-    passwordConfirm: passwordConfirm,
-    first_name: first_name,
-    last_name: last_name,
-    role: role,
-  };
+export const Auth = {
+  login: async (username: string, password: string) => {
+    const authData = await pb.collection("users").authWithPassword(
+      username,
+      password
+    );
 
-  const record = await pb.collection("users").create(data);
+    if (authData) {
+      return authData;
+    }
+  },
 
-  if (record) {
-    return record;
+  isSessionValid: async (): Promise<boolean> => {
+    return pb.authStore.isValid;
+  },
+
+  getCurrentUserId: async () => {
+    return pb.authStore.model?.id;
+  },
+
+  getUserData: async (id: string) => {
+    const record = await pb.collection("users").getOne(id);
+
+    if (record) {
+      return record;
+    }
+  },
+
+  getCurrentUserRole: async () => {
+    return pb.authStore.model?.role;
   }
 };
 
-export const getCurrentUserId = async () => {
-  return pb.authStore.model?.id;
-};
+export const User = {
+  register: async (
+    email: string,
+    password: string,
+    passwordConfirm: string,
+    firstName: string,
+    lastName: string,
+    role: string
+  ) => {
+    const username = firstName + "_" + lastName;
 
-export const getUserData = async (id: string) => {
-  const record = await pb.collection("users").getOne(id);
+    const data: UserData = {
+      username,
+      email,
+      password,
+      passwordConfirm,
+      first_name: firstName,
+      last_name: lastName,
+      role
+    };
 
-  if (record) {
-    return record;
+    const record = await pb.collection("users").create(data);
+
+    if (record) {
+      return record;
+    }
   }
-};
-
-export const getCurrentUserRole = async () => {
-  return pb.authStore.model?.role;
 };
